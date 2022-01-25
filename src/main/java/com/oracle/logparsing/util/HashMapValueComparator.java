90e747cef471473059d13d751b42dfe88ec5cff2
achatzikonstantinou <alexia.chatzikonstantinou@sword-group.com>
2022-01-25 18:12:07 +0200

    Implement HashMapValueComparator for ordering hashmap values.

---
+++ src/main/java/com/oracle/logparsing/util/HashMapValueComparator.java
@@ -0,0 +1,11 @@
+package com.oracle.logparsing.util;
+
+import java.util.Comparator;
+import java.util.Map;
+
+public class HashMapValueComparator implements Comparator  {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return ((Comparable) ((Map.Entry) (o1)).getValue()).compareTo(((Map.Entry) (o2)).getValue());
+    }
+}
