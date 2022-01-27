72d3c62b588ae476b92ba7ea438fe07f4da57757
achatzikonstantinou <alexia.chatzikonstantinou@sword-group.com>
2022-01-27 11:40:55 +0200

    Update support for command line options.

--- src/main/java/com/oracle/logparsing/model/MetricsReport.java
+++ src/main/java/com/oracle/logparsing/model/MetricsReport.java
@@ -4,26 +4,29 @@ import com.oracle.logparsing.util.ConsoleColors;
 
 public class MetricsReport {
 
-    private NasaLogMetricsSingleton nasaLogMetricsSingleton = NasaLogMetricsSingleton.getInstance();
-
     private static final String SUCCESSFUL_REQ_PER_MESSAGE = "Percentage of successful requests: ";
     private static final String UNSUCCESSFUL_REQ_PER_MESSAGE = "Percentage of unsuccessful requests: ";
+    private NasaLogMetricsSingleton metricsSingleton = NasaLogMetricsSingleton.getInstance();
 
     public void produceMetricsReport() {
+        boolean isFlagProvided = metricsSingleton.isFlagOptionEnabled();
+
         /* Percentage of successful requests*/
-        System.out.printf(SUCCESSFUL_REQ_PER_MESSAGE + "%.4f%%\n",
-                nasaLogMetricsSingleton.computePercentage(true));
+        if (!isFlagProvided || (isFlagProvided && metricsSingleton.isSuccessPercentFlag()))
+            System.out.printf(SUCCESSFUL_REQ_PER_MESSAGE + "%.4f%%\n",
+                    metricsSingleton.computePercentage(true));
+
         /* Percentage of unsuccessful requests*/
-        System.out.printf(UNSUCCESSFUL_REQ_PER_MESSAGE + "%.4f%%\n",
-                nasaLogMetricsSingleton.computePercentage(false));
+        if (!isFlagProvided || isFlagProvided & metricsSingleton.isUnsuccessfulPercentFlag())
+            System.out.printf(UNSUCCESSFUL_REQ_PER_MESSAGE + "%.4f%%\n",
+                    metricsSingleton.computePercentage(false));
     }
 
-    public void printReportsTitle(){
-        System.out.println("\n"+
-                ConsoleColors.CYAN_BOLD +
-                "********************" + "\n" +
-                "*** Final Report ***" + "\n" +
-                "********************" +
-                ConsoleColors.RESET_COLOUR);
+    public void printReportsTitle() {
+        System.out.println("\n" + ConsoleColors.CYAN_BOLD +
+                        "********************" + "\n" +
+                        "*** Final Report ***" + "\n" +
+                        "********************"
+                        + ConsoleColors.RESET_COLOUR);
     }
 }
