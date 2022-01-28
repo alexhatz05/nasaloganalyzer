e68e278ac82bca4b45f98601755822dc92f7f13e
achatzikonstantinou <alexia.chatzikonstantinou@sword-group.com>
2022-01-28 19:28:47 +0200

    Minor update in ConsoleColors.

--- src/main/java/com/oracle/logparsing/util/ConsoleColors.java
+++ src/main/java/com/oracle/logparsing/util/ConsoleColors.java
@@ -2,9 +2,11 @@ package com.oracle.logparsing.util;
 
 public class ConsoleColors {
 
+    private ConsoleColors() {}
+
     public static final String RESET_COLOUR = "\u001B[0m";
     public static final String RED_COLOUR = "\u001B[31m";       // RED
     public static final String YELLOW_COLOUR = "\033[0;33m";    // YELLOW
-    public static final String YELLOW_BOLD = "\033[1;33m";    // BOLD YELLOW
+    public static final String YELLOW_BOLD = "\033[1;33m";      // BOLD YELLOW
     public static final String CYAN_BOLD = "\033[1;36m";        // BOLD CYAN
 }
