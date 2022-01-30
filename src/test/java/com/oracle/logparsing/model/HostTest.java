58635196f9e35e7b8ece22cc295e888c25d0af44
achatzikonstantinou <alexia.chatzikonstantinou@sword-group.com>
2022-01-30 16:26:34 +0200

    Implement unit test for HostTest.

--- src/test/java/com/oracle/logparsing/model/HostTest.java
+++ src/test/java/com/oracle/logparsing/model/HostTest.java
@@ -1,23 +1,22 @@
 package com.oracle.logparsing.model;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
 
 class HostTest {
 
     private Host host;
-    private List<LoggedRequest> loggedRequestList;
-    private NasaLogMetricsSingleton nasaLogMetricsSingleton;
 
     @BeforeEach
     void init() {
-        nasaLogMetricsSingleton = NasaLogMetricsSingleton.getInstance();
-        loggedRequestList = new ArrayList<>();
+        NasaLogMetricsSingleton nasaLogMetricsSingleton = NasaLogMetricsSingleton.getInstance();
+        List<LoggedRequest> loggedRequestList = new ArrayList<>();
         host = new Host("www.google.com");
 
         LoggedRequest lr1 = new LoggedRequest("www.google.com","GET \"/www.page1.com\" HTTP/1.0", 200);
@@ -27,6 +26,7 @@ class HostTest {
         LoggedRequest lr5 = new LoggedRequest("www.nasa.com", "GET \"/www.page5.com\" HTTP/1.0", 200);
         LoggedRequest lr6 = new LoggedRequest("www.google.com", "GET \"/www.page6.com\" HTTP/1.0", 200);
         LoggedRequest lr7 = new LoggedRequest("www.google.com", "GET \"/www.page7.com\" HTTP/1.0", 200);
+        LoggedRequest lr8 = new LoggedRequest("www.google.com","GET \"/www.page4.com\" HTTP/1.0", 200);
 
         loggedRequestList.add(lr1);
         loggedRequestList.add(lr2);
@@ -35,11 +35,19 @@ class HostTest {
         loggedRequestList.add(lr5);
         loggedRequestList.add(lr6);
         loggedRequestList.add(lr7);
+        loggedRequestList.add(lr8);
         nasaLogMetricsSingleton.setRequestObjs(loggedRequestList);
     }
 
     @Test
-    void findRequestsOfHost() {
+    void findRequestsOfHostTest() {
+        Map<String, Long> expectedRequestsByHost = new HashMap<>();
 
+        expectedRequestsByHost.put("\"/www.page4.com\"", 2L);
+        expectedRequestsByHost.put("\"/www.page1.com\"", 1L);
+        expectedRequestsByHost.put("\"/www.page6.com\"", 1L);
+        expectedRequestsByHost.put("\"/www.page7.com\"", 1L);
+
+        Assertions.assertEquals(expectedRequestsByHost, host.findRequestsOfHost());
     }
 }
